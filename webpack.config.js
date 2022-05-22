9c51988c2d78c8950bf79b25e909b71704db3813
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2022-05-22 16:10:16 +0100

    UP

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: '../Manager/assets/js/evecloud.min.js',
+    filename: './dist/bundle.js',
     path: path.resolve(__dirname),
     libraryTarget: 'umd',
   },
