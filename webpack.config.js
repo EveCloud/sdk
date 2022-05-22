27c297d533a42314825b83652b8d24c03324dbbd
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2022-05-22 21:27:51 +0100

    UP

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: './dist/bundle.js',
+    filename: '../dist/bundle.js',
     path: path.resolve(__dirname),
     libraryTarget: 'umd',
   },
