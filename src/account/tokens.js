a4999396b926dd6319a4fa7b69179c8aee656e76
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2022-05-23 11:23:38 +0100

    api token endpoints

--- src/account/tokens.js
+++ src/account/tokens.js
@@ -1,27 +1,56 @@
 const { request, APIError } = require('../request');
 
 async function getAccountTokens() {
-        return new Promise(async (resolve, reject) => {
-            await request.get(`/account/tokens`).then(response => {
-                resolve(response.data)
-            }).catch(error => {
-                reject(APIError(error))
-            })
+    return new Promise(async (resolve, reject) => {
+        await request.get(`/account/tokens`).then(response => {
+            resolve(response.data)
+        }).catch(error => {
+            reject(APIError(error))
         })
-    }
+    })
+}
 
 
-    async function createAccountToken(data) {
-        return new Promise(async (resolve, reject) => {
-            await request.post(`/account/tokens`, data).then(response => {
-                resolve(response.data)
-            }).catch(error => {
-                reject(APIError(error))
-            })
+async function createAccountToken(label) {
+    return new Promise(async (resolve, reject) => {
+        await request.post(`/account/tokens`, {
+            label: label
+        }).then(response => {
+            resolve(response.data)
+        }).catch(error => {
+            reject(APIError(error))
         })
+    })
+}
+
+async function updateAccountToken(id, label) {
+    return new Promise(async (resolve, reject) => {
+        await request.put(`/account/tokens`, {
+            id: id,
+            label: label
+        }).then(response => {
+            resolve(response.data)
+        }).catch(error => {
+            reject(APIError(error))
+        })
+    })
+}
+
+async function deleteAccountToken(id) {
+    return new Promise(async (resolve, reject) => {
+        await request.post(`/account/tokens`, {
+            id: id
+        }).then(response => {
+            resolve(response.data)
+        }).catch(error => {
+            reject(APIError(error))
+        })
+    })
 }
 
 module.exports = {
     getAccountTokens,
-    createAccountToken
+    createAccountToken,
+    updateAccountToken,
+    deleteAccountToken
 }
