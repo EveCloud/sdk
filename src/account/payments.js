1a8dc14f1a7559d5d5a8017dccc8847fdf6ca8e0
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2022-07-12 17:42:40 +0100

    Update payments.js

--- src/account/payments.js
+++ src/account/payments.js
@@ -3,6 +3,16 @@ const { request, APIError } = require('../request');
 async function getPaymentMethods() {
     return new Promise(async (resolve, reject) => {
         await request.get(`/v1/account/payment-methods`).then(response => {
+            resolve(response.data.data)
+        }).catch(error => {
+            reject(APIError(error))
+        })
+    })
+}
+
+async function getPaymentMethod(id) {
+    return new Promise(async (resolve, reject) => {
+        await request.get(`/v1/account/payment-methods/${id}`).then(response => {
             resolve(response.data)
         }).catch(error => {
             reject(APIError(error))
@@ -12,7 +22,7 @@ async function getPaymentMethods() {
 
 async function makePaymentMethodDefault(paymentId) {
     return new Promise(async (resolve, reject) => {
-        await request.post(`/v1/account/payment-methods/` + paymentId + `/make-default`).then(response => {
+        await request.post(`/v1/account/payment-methods/${paymentId}/make-default`).then(response => {
             resolve(response.data)
         }).catch(error => {
             reject(APIError(error))
@@ -52,6 +62,7 @@ async function getStripePublicKey() {
 
 module.exports = {
     getPaymentMethods,
+    getPaymentMethod,
     addPaymentMethod,
     getStripePublicKey,
     deletePaymentMethod,
