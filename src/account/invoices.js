d4dfbcfd69c551c1a76a71e245b13a116435771c
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2022-07-12 17:40:54 +0100

    Update invoices.js

--- src/account/invoices.js
+++ src/account/invoices.js
@@ -1,16 +1,16 @@
 const { request, APIError } = require('../request');
 
-async function getAccountInvoices() {
+async function getInvoices() {
     return new Promise(async (resolve, reject) => {
         await request.get(`/v1/account/invoices`).then(response => {
-            resolve(response.data)
+            resolve(response.data.data)
         }).catch(error => {
             reject(APIError(error))
         })
     })
 }
 
-async function getInvoiceData(invoiceID) {
+async function getInvoice(invoiceID) {
     return new Promise(async (resolve, reject) => {
         await request.get(`/v1/account/invoices/` + invoiceID).then(response => {
             resolve(response.data)
@@ -21,6 +21,6 @@ async function getInvoiceData(invoiceID) {
 }
 
 module.exports = {
-    getAccountInvoices,
-    getInvoiceData
+    getInvoices,
+    getInvoice
 }
