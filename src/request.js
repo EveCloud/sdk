5d0f5e04170ccddaf546f5a0d73606492a551806
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2022-12-10 17:28:03 +0000

    more timeout

--- src/request.js
+++ src/request.js
@@ -7,7 +7,7 @@ const request = Axios.create({
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     },
-    timeout: '20000',
+    timeout: '50000',
     timeoutErrorMessage: 'Internal Server Error',
 })
 
