37ff06707f231f53aeee65708effc870e9b2509e
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-05-14 13:09:55 +0100

    utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -31,26 +31,12 @@ async function getPlans() {
 
 
 /**
- * Get Workers
- * @returns {Promise<String>} Workers
+ * Get Instances
+ * @returns {Promise<String>} Instances
  */
-async function getWorkers() {
+async function getInstances() {
     return new Promise(async (resolve, reject) => {
-        await request.get(`/workers`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
-    })
-}
-
-/**
- * Get Builders
- * @returns {Promise<String>} Builders
- */
-async function getBuilders() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/builders`).then(response => {
+        await request.get(`/instances`).then(response => {
             resolve(response.data)
         }).catch(error => {
             reject(APIError(error))
@@ -72,25 +58,8 @@ async function getRegions() {
     })
 }
 
-/**
- * Get Stats
- * @returns {Promise<String>} Stats
- */
-async function getStats() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/stats`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
-    })
-}
-
 module.exports = {
     getStripePublicKey,
-    getWorkers,
     getRegions,
-    getBuilders,
-    getStats,
     getPlans
 }
