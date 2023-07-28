8b73e99742fab7662010d1b417074a03fdda8661
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-28 12:15:38 +0100

    Update twoFactor.js

--- src/account/twoFactor.js
+++ src/account/twoFactor.js
@@ -1,42 +1,64 @@
 const { request, APIError } = require('../request')
 
-
-async function getTFA() {
-    return new Promise(async (resolve, reject) => {
-        await request.post(`/v1/account/two-factor`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name create
+ * @description Create two-factor authentication token
+ * @namespace Account
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Account/operation/TwoFactor.Create
+ * @returns {Promise<String>} Success message
+ */
+async function create () {
+  return new Promise((resolve, reject) => {
+    request.post('/v1/account/two-factor').then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
-async function verifyTFA(code) {
-    return new Promise(async (resolve, reject) => {
-        await request.put(`/v1/account/two-factor`, {
-            code: code
-        }).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name enable
+ * @description Enable two-factor authentication
+ * @namespace Account
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Account/operation/TwoFactor.Enable
+ * @param {String} code Two-factor authentication code
+ * @returns {Promise<String>} Success message
+ */
+async function enable (code) {
+  return new Promise((resolve, reject) => {
+    request.put('/v1/account/two-factor', {
+      code
+    }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
-async function disableTFA(code) {
-    return new Promise(async (resolve, reject) => {
-        await request.post(`/v1/account/two-factor/disable`, {
-            code: code
-        }).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name disable
+ * @description Disable two-factor authentication
+ * @namespace Account
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Account/operation/TwoFactor.Disable
+ * @param {String} code Two-factor authentication code
+ * @returns {Promise<String>} Success message
+ */
+async function disable (code) {
+  return new Promise((resolve, reject) => {
+    request.delete('/v1/account/two-factor', {
+      code
+    }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 module.exports = {
-    getTFA,
-    verifyTFA,
-    disableTFA
+  create,
+  enable,
+  disable
 }
