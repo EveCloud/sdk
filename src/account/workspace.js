321f35ca66696ece25a8af47241accf66003b00b
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-28 12:21:01 +0100

    Update workspace.js

--- src/account/workspace.js
+++ src/account/workspace.js
@@ -5,51 +5,49 @@ const { request, APIError } = require('../request')
  * @private For dashboard use only
  * @returns {Promise<Object>} Workspace object
  */
-async function getCurrent() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/account/workspace`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function getCurrent () {
+  return new Promise((resolve, reject) => {
+    request.get('/v1/account/workspace').then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 /**
  * Update the current workspace's
  * @private For dashboard use only
  * @param {String} id Workspace ID
- * @returns {Promise<Object>} Workspace object
  * @returns {Promise<String>} Message.
  */
-async function updateCurrent(id) {
-    return new Promise(async (resolve, reject) => {
-        await request.post(`/v1/account/workspace`, { id }).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function updateCurrent (id) {
+  return new Promise((resolve, reject) => {
+    request.post('/v1/account/workspace', { id }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 /**
  * Get all workspaces
  * @private For dashboard use only
  * @returns {Promise<Object>} Workspace object
- * @returns {Promise<String>} Message.
  */
-async function getAll() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/account/workspaces`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function getAll () {
+  return new Promise((resolve, reject) => {
+    request.get('/v1/account/workspaces').then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 module.exports = {
-    getCurrent,
-    updateCurrent,
-    getAll
+  getCurrent,
+  updateCurrent,
+  getAll
 }
