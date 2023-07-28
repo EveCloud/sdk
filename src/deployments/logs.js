a6804bcc9909615487b77e011e32284b233f9145
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-28 13:01:11 +0100

    Update logs.js

--- src/deployments/logs.js
+++ src/deployments/logs.js
@@ -1,20 +1,60 @@
 const { request, APIError } = require('../request')
 
-async function build(id, service, deployment, workspaceID, params) {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/projects/${id}/services/${service}/deployments/${deployment}/logs/build`, {
-            params: {
-                workspaceID: workspaceID,
-                ...params
-            },
-        }).then(response => {
-            resolve(response.data.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name build
+ * @description Get deployment build logs
+ * @namespace Deployments
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Deployments/operation/Logs.Build
+ * @param {String} id Project ID
+ * @param {String} service Service ID
+ * @param {String} deployment Deployment ID
+ * @param {String} workspaceID Workspace ID
+ * @param {String} params Parameters
+ * @returns {Promise<Object>} Logs
+ */
+async function build (id, service, deployment, workspaceID, params) {
+  return new Promise((resolve, reject) => {
+    request.get(`/v1/projects/${id}/services/${service}/deployments/${deployment}/logs/build`, {
+      params: {
+        workspaceID,
+        ...params
+      }
+    }).then(response => {
+      resolve(response.data.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
+}
+
+/**
+ * @name runtime
+ * @description Get deployment runtime logs
+ * @namespace Deployments
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Deployments/operation/Logs.Runtime
+ * @param {String} id Project ID
+ * @param {String} service Service ID
+ * @param {String} deployment Deployment ID
+ * @param {String} workspaceID Workspace ID
+ * @param {String} params Parameters
+ * @returns {Promise<Object>} Logs
+ */
+async function runtime (id, service, deployment, workspaceID, params) {
+  return new Promise((resolve, reject) => {
+    request.get(`/v1/projects/${id}/services/${service}/deployments/${deployment}/logs/runtime`, {
+      params: {
+        workspaceID,
+        ...params
+      }
+    }).then(response => {
+      resolve(response.data.data)
+    }).catch(error => {
+      reject(APIError(error))
+    })
+  })
 }
 
 module.exports = {
-    build
+  build,
+  runtime
 }
