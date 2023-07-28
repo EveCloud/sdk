4101a2547b8b2246d4d26fac053db8d06ee7f150
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-28 11:59:44 +0100

    Update integrations.js

--- src/account/integrations.js
+++ src/account/integrations.js
@@ -1,8 +1,11 @@
 const { request, APIError } = require('../request')
 
 /**
- * Get github repositories
- * @returns {Promise<Object>} The github repositories
+ * @name getGitHubRepositories
+ * @description Get GitHub repositories
+ * @namespace Account
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Account/operation/Integrations.GitHub.Repositories
+ * @returns {Promise<Object>} GitHub repositories
  */
 async function getGitHubRepositories() {
     return new Promise(async (resolve, reject) => {
@@ -14,6 +17,29 @@ async function getGitHubRepositories() {
     })
 }
 
+/**
+ * @name connectGitHub
+ * @description Connect GitHub to your account
+ * @namespace Account
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Account/operation/Integrations.GitHub.Connect
+ * @param {String} code The code returned from GitHub
+ * @param {String} installationId The installation ID returned from GitHub
+ * @returns {Promise<String>} Success message
+ */
+async function connectGitHub(code, installationId) {
+    return new Promise(async (resolve, reject) => {
+        await request.post(`/v1/account/integrations/github/connect`, {
+            code: code,
+            installationId: installationId
+        }).then(response => {
+            resolve(response.data)
+        }).catch(error => {
+            reject(APIError(error))
+        })
+    })
+}
+
 module.exports = {
-    getGitHubRepos
+    getGitHubRepositories,
+    connectGitHub
 }
