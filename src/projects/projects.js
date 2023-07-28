ca96314eca43d22220de7d4b75bcf47fcdca207a
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-28 13:11:01 +0100

    Update projects.js

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,65 +1,128 @@
 const { request, APIError } = require('../request')
 
-async function list(workspaceID, params) {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/projects`, {
-            params: {
-                workspaceID: workspaceID,
-                ...params
-            }
-        }).then(response => {
-            resolve(response.data.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name list
+ * @description Get projects
+ * @namespace Projects
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Projects/operation/Projects.List
+ * @param {String} workspaceID Workspace ID
+ * @returns {Promise<Object>} Projects
+ */
+async function list (workspaceID, params) {
+  return new Promise((resolve, reject) => {
+    request.get('/v1/projects', {
+      params: {
+        workspaceID,
+        ...params
+      }
+    }).then(response => {
+      resolve(response.data.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
-async function get(id, workspaceID) {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/projects/${id}`, {
-            params: {
-                workspaceID: workspaceID
-            }
-        }).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name get
+ * @description Get project
+ * @namespace Projects
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Projects/operation/Projects.Get
+ * @param {String} id Project ID
+ * @param {String} workspaceID Workspace ID
+ * @returns {Promise<Object>} Project
+ */
+async function get (id, workspaceID) {
+  return new Promise((resolve, reject) => {
+    request.get(`/v1/projects/${id}`, {
+      params: {
+        workspaceID
+      }
+    }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
-async function create(id, data) {
-    return new Promise(async (resolve, reject) => {
-        await request.post(`/v1/projects/`, data, {
-            params: {
-                workspaceID: id
-            }
-        }).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name create
+ * @description Create project
+ * @namespace Projects
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Projects/operation/Projects.Create
+ * @param {String} workspaceID Workspace ID
+ * @param {Object} data Project data
+ * @returns {Promise<Object>} Project
+ */
+async function create (workspaceID, data) {
+  return new Promise((resolve, reject) => {
+    request.post('/v1/projects/', data, {
+      params: {
+        workspaceID
+      }
+    }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
-async function update(id, data, workspaceID) {
-    return new Promise(async (resolve, reject) => {
-        await request.put(`/v1/projects/${id}`, data, {
-            params: {
-                workspaceID: workspaceID
-            }
-        }).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+/**
+ * @name update
+ * @description Update project
+ * @namespace Projects
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Projects/operation/Projects.Update
+ * @param {String} id Project ID
+ * @param {Object} data Project data
+ * @param {String} workspaceID Workspace ID
+ * @returns {Promise<Object>} Project
+ */
+async function update (id, data, workspaceID) {
+  return new Promise((resolve, reject) => {
+    request.put(`/v1/projects/${id}`, data, {
+      params: {
+        workspaceID
+      }
+    }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
+}
+
+/**
+ * @name remove
+ * @description Remove project
+ * @namespace Projects
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Projects/operation/Projects.Remove
+ * @param {String} id Project ID
+ * @param {Boolean} confirm Confirm removal
+ * @param {String} workspaceID Workspace ID
+ * @returns {Promise<Object>} Success message
+ */
+async function remove (id, confirm, workspaceID) {
+  return new Promise((resolve, reject) => {
+    request.delete(`/v1/projects/${id}`, {
+      confirm
+    }, {
+      params: {
+        workspaceID
+      }
+    }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
+    })
+  })
 }
 
 module.exports = {
-    list,
-    get,
-    create,
-    update
+  list,
+  get,
+  create,
+  update,
+  remove
 }
