3c7b65681b342f116c64b06b43bc315938ce4956
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-28 12:04:28 +0100

    Update security.js

--- src/account/security.js
+++ src/account/security.js
@@ -5,22 +5,21 @@ const { request, APIError } = require('../request')
  * @description Get your authentication events
  * @namespace Account
  * @link https://docs.evecloud.xyz/reference/rest-api#tag/Account/operation/Security.GetEvents
- * @param {String} code The code returned from GitHub
- * @param {String} installationId The installation ID returned from GitHub
+ * @param {String} params Parameters
  * @returns {Promise<Object>} Authentication events
  */
-async function getEvents(params) {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/account/security/events`, {
-            params: params || {}
-        }).then(response => {
-            resolve(response.data.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function getEvents (params) {
+  return new Promise((resolve, reject) => {
+    request.get('/v1/account/security/events', {
+      params: params || {}
+    }).then(response => {
+      resolve(response.data.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 module.exports = {
-    getEvents
+  getEvents
 }
