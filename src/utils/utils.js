a6c8eeaf763c14915fc96044f4bde7b2e16cc194
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-29 10:23:09 +0100

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,66 +1,75 @@
 const { request, APIError } = require('../request')
 
 /**
- * Get Stripe Public Key
- * @private Manager Endpoint only
- * @returns {Promise<String>} Stripe Public Key
+ * @name getStripePublicKey
+ * @description Get Stripe Public Key
+ * @namespace Utils
+ * @returns {Promise<Object>} Stripe Public Key
  */
-async function getStripePublicKey() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/payments/stripe/public-key`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function getStripePublicKey () {
+  return new Promise((resolve, reject) => {
+    request.get('/v1/payments/stripe/public-key').then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 /**
- * Get Plans
- * @returns {Promise<String>} Plans
+ * @name getPlans
+ * @description Get Plans
+ * @namespace Utils
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Utils/operation/Utils.Plans
+ * @returns {Promise<Object>} Plans
  */
-async function getPlans() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/plans`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function getPlans () {
+  return new Promise((resolve, reject) => {
+    request.get('/plans').then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
-}
-
-
-/**
- * Get Instances
- * @returns {Promise<String>} Instances
- */
-async function getInstances() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/instances`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
-    })
+  })
 }
 
 /**
- * Get Regions
- * @returns {Promise<String>} Regions
+ * @name getInstances
+ * @description Get Instances
+ * @namespace Utils
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Utils/operation/Utils.Instances
+ * @returns {Promise<Object>} Instances
  */
-async function getRegions() {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/regions`).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function getInstances () {
+  return new Promise((resolve, reject) => {
+    request.get('/instances').then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
+}
+
+/**
+ * @name getRegions
+ * @description Get Regions
+ * @namespace Utils
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Utils/operation/Utils.Regions
+ * @returns {Promise<Object>} Regions
+ */
+async function getRegions () {
+  return new Promise((resolve, reject) => {
+    request.get('/regions').then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
+    })
+  })
 }
 
 module.exports = {
-    getStripePublicKey,
-    getRegions,
-    getPlans,
-    getInstances
+  getStripePublicKey,
+  getRegions,
+  getPlans,
+  getInstances
 }
