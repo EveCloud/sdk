657d6eb6558ed1f9ec1b149b64e3b6de0661e8cb
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-29 10:37:41 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   mode: 'production',
@@ -7,12 +7,12 @@ module.exports = {
   output: {
     filename: './dist/bundle.js',
     path: path.resolve(__dirname),
-    libraryTarget: 'umd',
+    libraryTarget: 'umd'
   },
   plugins: [
     new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
+      $: 'jquery',
+      jQuery: 'jquery'
     })
-],
-};
+  ]
+}
