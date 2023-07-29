a580d146448b69dff393555457f728978284f805
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-29 10:23:11 +0100

    Update invoices.js

--- src/workspaces/invoices.js
+++ src/workspaces/invoices.js
@@ -1,37 +1,42 @@
 const { request, APIError } = require('../request')
 
 /**
- * Ger the current user's account invoices
+ * @name list
+ * @description List workpaces invoices
+ * @namespace Workspaces
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Workspaces/operation/Invocies.List
  * @param {String} id Workspace ID
- * @returns {Promise<Object>} Invoice List object
+ * @returns {Promise<Object>} Invoices
  */
-async function list(id) {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/workspaces/${id}/invoices`).then(response => {
-            resolve(response.data.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function list (id) {
+  return new Promise((resolve, reject) => {
+    request.get(`/v1/workspaces/${id}/invoices`).then(response => {
+      resolve(response.data.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 /**
- * Get account invoice
+ * @name get
+ * @description Get workpace invoice
+ * @namespace Workspaces
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Workspaces/operation/Invocies.Get
  * @param {String} id Workspace ID
- * @param {String} invoiceID Invoice ID
- * @returns {Promise<Object>} Invoice object
+ * @returns {Promise<Object>} Invoice
  */
-async function get(id, invoiceID) {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/workspaces/${id}/invoices/` + invoiceID).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function get (id, invoiceID) {
+  return new Promise((resolve, reject) => {
+    request.get(`/v1/workspaces/${id}/invoices/` + invoiceID).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 module.exports = {
-    list,
-    get
+  list,
+  get
 }
