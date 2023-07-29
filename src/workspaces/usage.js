3baed5c06005bbd3cfe7c15b6ddea03381d52688
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-29 10:35:53 +0100

    Update usage.js

--- src/workspaces/usage.js
+++ src/workspaces/usage.js
@@ -1,24 +1,30 @@
 const { request, APIError } = require('../request')
 
 /**
- * Get usage
- * @returns {Promise<Object>} Usage object
+ * @name get
+ * @description Get workpace usage
+ * @namespace Workspaces
+ * @link https://docs.evecloud.xyz/reference/rest-api#tag/Workspaces/operation/Usage.Get
+ * @param {String} id Workspace ID
+ * @param {String} month Month
+ * @param {String} year Year
+ * @returns {Promise<Object>} Usage
  */
-async function get(id, month, year) {
-    return new Promise(async (resolve, reject) => {
-        await request.get(`/v1/workspaces/${id}/usage`, {
-            params: {
-                month,
-                year
-            }
-        }).then(response => {
-            resolve(response.data)
-        }).catch(error => {
-            reject(APIError(error))
-        })
+async function get (id, month, year) {
+  return new Promise((resolve, reject) => {
+    request.get(`/v1/workspaces/${id}/usage`, {
+      params: {
+        month,
+        year
+      }
+    }).then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(APIError(error))
     })
+  })
 }
 
 module.exports = {
-    get
+  get
 }
