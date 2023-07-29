6e402b969691530d7390c0817e6e5b15ec88c621
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2023-07-29 10:35:50 +0100

    Update request.js

--- src/request.js
+++ src/request.js
@@ -1,31 +1,31 @@
 const Axios = require('axios').default
 
 const request = Axios.create({
-    baseURL: 'https://api.evecloud.xyz',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/vnd.evecloud.v1+json',
-    },
-    timeout: '50000',
-    timeoutErrorMessage: 'Internal Server Error',
+  baseURL: 'https://api.evecloud.xyz',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/vnd.evecloud.v1+json'
+  },
+  timeout: '50000',
+  timeoutErrorMessage: 'Internal Server Error'
 })
 
 /**
  * Set Authorization header
  * @param {String} token Access token
- * @returns {void} 
+ * @returns {void}
  */
 const setToken = (token) => {
-    return request.interceptors.request.use((config) => {
-        return {
-            ...config,
-            headers: {
-                ...config.headers,
-                Authorization: `Bearer ${token}`,
-            },
-        };
-    });
-};
+  return request.interceptors.request.use((config) => {
+    return {
+      ...config,
+      headers: {
+        ...config.headers,
+        Authorization: `Bearer ${token}`
+      }
+    }
+  })
+}
 
 /**
  * Set URL
@@ -33,39 +33,39 @@ const setToken = (token) => {
  * @returns {void}
  */
 const setURL = (url) => {
-    return request.interceptors.request.use((config) => {
-        return {
-            ...config,
-            baseURL: url,
-        };
-    });
-};
+  return request.interceptors.request.use((config) => {
+    return {
+      ...config,
+      baseURL: url
+    }
+  })
+}
 
 const APIError = (error) => {
-    if (error.code === "ECONNREFUSED") {
-        return "Internal Server Error"
-    }
-    if (error.code === "ECONNABORTED") {
-        return "The request has been aborted."
-    }
-    if (error.code === "ETIMEDOUT"){
-        return "Unable to connect to the server."
-    }
-    if (error.response.status === 502) {
-        return "Bad Gateway"
-    }
-    if (error.response.status === 403) {
-        return error.response.data.message
-    }
-    if (error.response.data.message) {
-        return error.response.data.message
-    }
-    return "Internal Server Error"
-};
+  if (error.code === 'ECONNREFUSED') {
+    return 'Internal Server Error'
+  }
+  if (error.code === 'ECONNABORTED') {
+    return 'The request has been aborted.'
+  }
+  if (error.code === 'ETIMEDOUT') {
+    return 'Unable to connect to the server.'
+  }
+  if (error.response.status === 502) {
+    return 'Bad Gateway'
+  }
+  if (error.response.status === 403) {
+    return error.response.data.message
+  }
+  if (error.response.data.message) {
+    return error.response.data.message
+  }
+  return 'Internal Server Error'
+}
 
 module.exports = {
-    request,
-    setToken,
-    APIError,
-    setURL,
+  request,
+  setToken,
+  APIError,
+  setURL
 }
